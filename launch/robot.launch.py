d16b45217b283d99e8e31a7fe361809995e33854
mohamedhelmy2002 <mahmoudmohamedhelmy8@gmail.com>
2025-09-23 20:39:54 +0300

    dd

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,30 +1,41 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import Command, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 import os
 
-def generate_launch_description():
-    pkg_share = FindPackageShare('test_robot').find('test_robot')
-    urdf_file = PathJoinSubstitution([pkg_share, 'urdf', 'robot_.urdf.xacro'])
+from ament_index_python.packages import get_package_share_directory
 
-    # Use a simpler Command approach without FindExecutable
-    robot_description_content = Command(['xacro ', urdf_file])
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('test_robot')
 
     return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{
-                'robot_description': robot_description_content
-            }]
+        # 1. Launch RViz/display.launch.py with use_sim_time true
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_path, 'launch', 'display.launch.py')
+            ),
+            launch_arguments={'use_sim_time': 'true'}.items() 
         ),
-        Node(
-            package='joint_state_publisher_gui', 
-            executable='joint_state_publisher_gui'
+         # 2. Launch Gazebo (empty world)
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('gazebo_ros'),
+                    'launch',
+                    'gazebo.launch.py'
+                )
+            ),
+            # you can add world/verbose args here
+            # launch_arguments={'world': '/path/to/world.sdf', 'verbose':'true'}.items()
         ),
-        Node(
-            package='rviz2',
-            executable='rviz2'
-        )
+        # 3. Spawn your robot into Gazebo
+        Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'robot'],
+                        output='screen')
+                        
     ])
